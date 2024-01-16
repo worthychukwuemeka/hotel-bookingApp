77150bf2f87e6f053ffc464f768b4ea75dc1486c
Danniefedev874 <112968099+Danniefedev874@users.noreply.github.com>
2024-01-16 19:47:06 +0100

    Update HotelController.java; content added to HotelController.

--- src/main/java/com/example/hotelbookingapp/controller/HotelController.java
+++ src/main/java/com/example/hotelbookingapp/controller/HotelController.java
@@ -1,4 +1,41 @@
 package main.java.com.example.hotelbookingapp.controller;
 
+import com.example.hotelbookingapp.model.Hotel;
+import com.example.hotelbookingapp.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/hotels")
 public class HotelController {
+
+    @Autowired
+    private HotelService hotelService;
+
+    @GetMapping
+    public List<Hotel> getAllHotels() {
+        return hotelService.getAllHotels();
+    }
+
+    @GetMapping("/{id}")
+    public Hotel getHotelById(@PathVariable Long id) {
+        return hotelService.getHotelById(id);
+    }
+
+    @PostMapping
+    public Hotel createHotel(@RequestBody Hotel hotel) {
+        return hotelService.createHotel(hotel);
+    }
+
+    @PutMapping("/{id}")
+    public Hotel updateHotel(@PathVariable Long id, @RequestBody Hotel hotel) {
+        return hotelService.updateHotel(id, hotel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteHotel(@PathVariable Long id) {
+        hotelService.deleteHotel(id);
+    }
 }
