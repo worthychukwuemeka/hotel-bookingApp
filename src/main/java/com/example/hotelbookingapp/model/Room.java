2ff116b5058258061a277bc75e5729b02b2b8526
SarahDoma <108196951+SarahDoma@users.noreply.github.com>
2024-01-16 23:52:31 +0100

    Update Room.java

--- src/main/java/com/example/hotelbookingapp/model/Room.java
+++ src/main/java/com/example/hotelbookingapp/model/Room.java
@@ -1,29 +1,62 @@
-
 package com.example.hotelbookingapp.model;
 
 import jakarta.persistence.*;
+import java.util.Objects;
 
 @Entity
-@Table(name = "rooms")
-public class Room {
+@Table(name = "hotels")
+public class Hotel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String roomNumber;
+    private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "hotel_id", nullable = false)
-    private Hotel hotel;
+    @Column(nullable = false)
+    private String address;
 
-    // Additional fields
-    private boolean isBooked;
+    @Column(nullable = false)
+    private String city;
 
-// Other fields and methods...
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String website;
+
+    @Column(nullable = false)
+    private int starRating;
+
+    @Column(nullable = false)
+    private String description;
+
+    public Hotel() {
+        // Default constructor
+    }
+
+    public Hotel(String name, String address, String city, String country, String postalCode,
+                 String phoneNumber, String website, int starRating, String description) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.postalCode = postalCode;
+        this.phoneNumber = phoneNumber;
+        this.website = website;
+        this.starRating = starRating;
+        this.description = description;
+    }
 
     // Getters and setters...
+
     public Long getId() {
         return id;
     }
@@ -32,31 +65,114 @@ public class Room {
         this.id = id;
     }
 
-    public String getRoomNumber() {
-        return roomNumber;
+    public String getName() {
+        return name;
     }
 
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Hotel getHotel() {
-        return hotel;
+    public String getAddress() {
+        return address;
     }
 
-    public void setHotel(Hotel hotel) {
-        this.hotel = hotel;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
-    public boolean isBooked() {
-        return isBooked;
+    public String getCity() {
+        return city;
     }
 
-    public void setBooked(boolean booked) {
-        isBooked = booked;
+    public void setCity(String city) {
+        this.city = city;
     }
 
-// toString, hashCode, equals methods...
-// Override these methods as needed
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", website='" + website + '\'' +
+                ", starRating=" + starRating +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, city, country, postalCode, phoneNumber, website, starRating, description);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Hotel hotel = (Hotel) obj;
+        return Objects.equals(id, hotel.id) &&
+                Objects.equals(name, hotel.name) &&
+                Objects.equals(address, hotel.address) &&
+                Objects.equals(city, hotel.city) &&
+                Objects.equals(country, hotel.country) &&
+                Objects.equals(postalCode, hotel.postalCode) &&
+                Objects.equals(phoneNumber, hotel.phoneNumber) &&
+                Objects.equals(website, hotel.website) &&
+                starRating == hotel.starRating &&
+                Objects.equals(description, hotel.description);
+    }
 
 }
