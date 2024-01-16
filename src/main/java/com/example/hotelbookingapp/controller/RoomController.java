23ab2e48114b69e4f28ffc86e798ae72bb9f0bd4
Danniefedev874 <112968099+Danniefedev874@users.noreply.github.com>
2024-01-16 19:59:49 +0100

    Update RoomController.java; content added.

--- src/main/java/com/example/hotelbookingapp/controller/RoomController.java
+++ src/main/java/com/example/hotelbookingapp/controller/RoomController.java
@@ -1,4 +1,41 @@
-package main.java.com.example.hotelbookingapp.controller;
+package com.example.hotelbookingapp.controller;
 
-public class RoomController {
+import com.example.hotelbookingapp.model.User;
+import com.example.hotelbookingapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
